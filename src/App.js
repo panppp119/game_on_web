234584579217ca7062223a72626605b5604e1a4f
PanPPP <panppp@PanPPPs-MacBook-Pro.local>
2019-08-08 00:08:59 +0700

    Remove unused file and add Home and NotFound pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import asyncComponent from './components/AsyncComponent';
 
-export default App;
+const AsyncHome = asyncComponent(() => import('pages/Home'));
+const AsyncNotFound = asyncComponent(() => import('pages/NotFound'));
+
+export default ({ childProps }) => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={AsyncHome} props={childProps} />
+
+      {/* unmatched routes */}
+      <Route component={AsyncNotFound} />
+    </Switch>
+  </Router>
+);
